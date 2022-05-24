d16413be0b2c408dfd872c73ca23232a04e79c4d
mehmeturmac <mehmeturmac@gmail.com>
2022-05-24 10:36:30 +0300

    form updated

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function Form({ todos, setTodos, todoId }) {
+function Form({ todos, setTodos }) {
   const [form, setForm] = useState("");
 
   useEffect(() => {
